ce825b439b8e0979dd191e1e65a767efb18e4d0c
Scott Chu <scottchu@icloud.com>
2019-03-28 13:25:56 -0400

    Update NavigationSystem with common interface

--- source/elixir/lib/mars_rover/navigation_system.ex
+++ source/elixir/lib/mars_rover/navigation_system.ex
@@ -1,2 +1,23 @@
 defmodule MarsRover.NavigationSystem do
+  alias MarsRover.NavigationSystem.{Bound, Coordinate, Orientation}
+
+  @spec within_bound?(Bound.t(), Coordinate.t()) :: boolean()
+  def within_bound?(bound, coordinate) do
+    Bound.within?(bound, coordinate)
+  end
+
+  @spec outof_bound?(Bound.t(), Coordinate.t()) :: boolean()
+  def outof_bound?(bound, coordinate) do
+    Bound.outof?(bound, coordinate)
+  end
+
+  @spec move(Coordinate.t(), Orientation.t()) :: Coordinate.t()
+  def move(coordinate, orientation) do
+    Coordinate.move(coordinate, orientation)
+  end
+
+  @spec turn(Orientation.t(), Orientation.side()) :: Orientation.t()
+  def turn(orientation, to) do
+    Orientation.turn(orientation, to)
+  end
 end
